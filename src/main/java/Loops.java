d73b1f18339fcac7616171e5dea2bc6edd5fd254
Ktabibian <Kyle.tabibian@yahoo.com>
2022-10-04 00:35:13 -0400

    20questions

--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -1,3 +1,6 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class Loops {
     //Q1
     public static int getLastIndex(String[] names) {
@@ -43,7 +46,7 @@ public class Loops {
         String oddNumbers = "";
         for(int i = 0; i < numbers.length; i++){
             if(numbers[i] % 2 != 0){
-                oddNumbers = oddNumbers + String.valueOf(numbers[i]) + " ";
+                oddNumbers += numbers[i] + " ";
             }
         }
         return oddNumbers;
@@ -53,7 +56,7 @@ public class Loops {
         String evenNumbers = "";
         for(int i = 0; i < numbers.length; i++){
             if(numbers[i] % 2 == 0){
-                evenNumbers = evenNumbers + String.valueOf(numbers[i]) + " ";
+                evenNumbers += numbers[i] + " ";
             }
         }
         return evenNumbers;
@@ -112,11 +115,86 @@ public class Loops {
                 count++;
             }
         }
+        return count + 1;
+    }
+    //Question 16
+    //A, E, I, O, U, and sometimes Y
+    public static int vowelsCounter(String str){
+        int count = 0;
+        str = str.toLowerCase();
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+                count++;
+            }
+        }
         return count;
+    }
+    //Question 17
+    //Swap the first element in an array with the last element in an array and return
+    public static String[] swap(String[] stringArray){
+        String firstElement = stringArray[0];
+        stringArray[0] = stringArray[stringArray.length - 1];
+        stringArray[stringArray.length - 1] = firstElement;
+        return stringArray;
+    }
+    //Question 18
+    /*
+     * Given the following legend
+     *
+     * f   >>   7
+     * s   >>   $
+     * 1   >>   !
+     * a   >>   @
+     *
+     * your method should replace any character represented by a key in the legend, with its corresponding value.
+     * Input  = "The Farmer went to the store to get 1 dollar's worth of fertilizer"
+     * Output = "The 7@rmer went to the $tore to get ! doll@r'$ worth of 7ertilizer"
+     *
+     * output = The 7@rmer went to the $tore to get ! doll@r'$ worth o7 7ertilizer
+     */
+    public static String replaceCharacters(String str){
+        str = str.replaceAll("[fF]", "7");
+        str = str.replace('s', '$');
+        str = str.replace('1', '!');
+        str = str.replace('a', '@');
+        return str;
+    }
+    //Question 19
+            //" The small brown dog hopped the fence " becomes " The Wu Tang Wu Hopped Wu Fence "
+    public static String replaceWuTangTwoThreeDivisible(String stringInput){
+        String words = "";
+        String[] clan = stringInput.split(" ");
+        for(int i = 1; i < clan.length; i++){
+            if(i % 2 == 0){
+                clan[i] = "Wu";
+            }else if(i % 3 == 0){
+                clan[i] = "Tang";
+            }
+        }
+        for(int j = 0; j < clan.length; j++){
+            words += clan[j] + " ";
+        }
+        return words;
+    }
+    //Question 20
+    public static String createStringOfFibonnaciUpToMax(int maxnumber){
+        int a = 0;
+        int b = 1;
+        String c = a + " " + b;
+        for(int i = 0; i < maxnumber; i++){
+            int d = a + b;
+            a = b;
+            b = d;
+            if(d >= maxnumber){
+                break;
+            }
+            c = c + " " + d;
+        }
+        return c;
     }
 
     public static void main(String[] args) {
-        String[] names = {"How", "Do", "I", "Code", "This", "Shit"};
+        String[] names = {"How", "Do", "I", "Code", "This", "Stuff"};
         int[] numbers = {2, 3, 6, 9, 10};
         System.out.println(getLastIndex(names));
         System.out.println(getSecondToLastIndex(names));
@@ -127,11 +205,16 @@ public class Loops {
         System.out.println(getAverage(numbers));
         System.out.println(extractAllOddNumbers(numbers));
         System.out.println(extractAllEvenNumbers(numbers));
-        System.out.println(contains(names, "Balls"));
+        System.out.println(contains(names, "Bunny"));
         System.out.println(getIndexByElement(names, "This"));
         printOddNumbersInRange(2, 10);
-        System.out.println(printGivenStringTimesNumberGiven("Balls ", 3));
+        System.out.println(printGivenStringTimesNumberGiven("Bunny ", 3));
         System.out.println(repeatFirstThreeLetters("Difficult", 3));
         System.out.println(wordsInAStringCounter("I finally finished my flipping work"));
+        System.out.println(vowelsCounter("Alvin Flang"));
+        System.out.println(Arrays.toString(swap(names)));
+        System.out.println(replaceCharacters("The Farmer went to the store to get 1 dollar's worth of fertilizer"));
+        System.out.println(replaceWuTangTwoThreeDivisible(" The small brown dog hopped the fence "));
+        System.out.println(createStringOfFibonnaciUpToMax(40));
     }
 }
